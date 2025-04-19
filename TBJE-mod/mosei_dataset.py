Fix gradient issue with input data scaling

TBJE-mod/mosei_dataset.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import pickle
 import numpy as np
+import torch.nn.functional as F
 
 class MOSEIDataset(Dataset):
     def __init__(self, pickle_path, split, max_seqlen=50):
@@ -42,7 +43,12 @@ class MOSEIDataset(Dataset):
         return self.y.shape[0]
 
     def __getitem__(self, idx):
-        X = [torch.Tensor(X_elem[idx]) for X_elem in self.X_list]
+        X = [
+            torch.nan_to_num(
+                F.normalize(torch.Tensor(X_elem[idx]).to(dtype=torch.float64), p=2.0, dim=1, eps=1e-10)
+            ).to(dtype=torch.float32)
+            for X_elem in self.X_list
+        ]
         y = torch.Tensor(self.y[idx])
         return *X, y
 

TBJE-mod/tbje.py
@@ -43,7 +43,8 @@ class TBJEBlock(nn.Module):
         self.aud_slf_attn = nn.MultiheadAttention(internal_dim, n_heads, batch_first=True, bias=False, dropout=.2)
         ########################################################################
 
-    def forward(self, vid_data, txt_data, aud_data):
+    def forward(self, data_tuple):
+        vid_data, txt_data, aud_data = data_tuple
         # cross attention stage
         vid_ln_out = self.vid_ln1(vid_data)
         txt_ln_out = self.txt_ln1(txt_data)
@@ -103,7 +104,8 @@ class TBJENew(nn.Module):
         self.txt_proj_ln = nn.LayerNorm(internal_dim, bias=False)
         self.aud_proj_ln = nn.LayerNorm(internal_dim, bias=False)
 
-        self.block = TBJEBlock(internal_dim, mlp_hidden_dim, n_heads)
+        self.blocks = [TBJEBlock(internal_dim, mlp_hidden_dim, n_heads) for _ in range(n_layers)]
+        self.blocks = nn.Sequential(*self.blocks)
 
         self.final_proj1 = nn.Linear(3 * internal_dim, 128, bias=False)
         self.final_proj2 = nn.Linear(128, 1, bias=False)
@@ -118,7 +120,7 @@ class TBJENew(nn.Module):
         txt_data = self.txt_proj_ln(self.txt_initial_proj(txt_data.contiguous()))
         aud_data = self.aud_proj_ln(self.aud_initial_proj(aud_data.contiguous()))
 
-        vid_data, txt_data, aud_data = self.block(vid_data, txt_data, aud_data)
+        vid_data, txt_data, aud_data = self.blocks((vid_data, txt_data, aud_data))
         vid_data = vid_data[:, -1, :]
         txt_data = txt_data[:, -1, :]
         aud_data = aud_data[:, -1, :]